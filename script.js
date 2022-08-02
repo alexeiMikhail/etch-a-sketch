aa266e1587fe6d163c9ec16fe76085a04fa8d6c9
alexeiMikhail <leleuxc@gmail.com>
2022-08-02 11:03:14 -0500

    Add grid with event listener that changes bg color per cell on mouseover

---
+++ script.js
@@ -0,0 +1,31 @@
+var grid = {
+    x: 10,
+    y: 15,
+}
+
+makeGrid(grid.x, grid.y);
+
+function random_bg_color(element) {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    element.style.background = bgColor;
+}
+
+
+function makeGrid(x,y) {
+    for (let i = 0; i < y; i++) {
+        let row = document.createElement("div");
+        row.className = "row";
+        for (let j = 0; j < x; j++) {
+            let cell = document.createElement("div");
+            cell.className = "cell";
+            cell.addEventListener("mouseover", () => {
+                random_bg_color(cell);
+            })
+            row.appendChild(cell);
+        }
+        document.body.appendChild(row);
+    }    
+}
